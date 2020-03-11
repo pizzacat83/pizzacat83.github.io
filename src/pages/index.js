5fa8522c1893d4db61355415e5326e792a91b83d
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2020-03-11 22:00:30 +0900

    add spinner

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
-import React from "react"
+import React from 'react'
+import spinnerBase from '../assets/spinner_base.svg';
+import spinnerMouth from '../assets/spinner_mouth.svg';
+import spinnerStyle from '../assets/spinner.module.css';
 
-export default () => <div>Hello world!</div>
+export default () => (
+    <div className={spinnerStyle.spinner_outer}>
+    <div className={spinnerStyle.spinner_container}>
+        <img src={spinnerMouth} alt="" className={spinnerStyle.spinner_mouth}/>
+        <img src={spinnerBase} alt="spinner" className={spinnerStyle.spinner_base}/>
+    </div></div>
+);
