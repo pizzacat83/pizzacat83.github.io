eee7380a079c8f7ab545dca1b98f1302a6f7f2f2
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2020-03-12 22:44:54 +0900

    add title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,13 @@
 import React from "react";
+import Helmet from "react-helmet";
 import { SpinnerContainer } from "../components/spinner";
 
 export default () => (
-  <SpinnerContainer spinnerSize="30%" style={{ height: "100%" }} />
+  <div style={{ height: "100%" }}>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>pizzacat83</title>
+    </Helmet>
+    <SpinnerContainer spinnerSize="30%" style={{ height: "100%" }} />
+  </div>
 );
