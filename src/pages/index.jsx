b833bd8558bb6ccf1b1bc257f4b02a91261d9fb0
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2020-03-12 14:27:49 +0900

    use ESLint

---
+++ src/pages/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import spinnerBase from '../assets/spinner_base.svg';
+import spinnerMouth from '../assets/spinner_mouth.svg';
+import spinnerStyle from '../assets/spinner.module.css';
+
+export default () => (
+  <div className={spinnerStyle.spinner_outer}>
+    <div className={spinnerStyle.spinner_container}>
+      <img src={spinnerMouth} alt="" className={spinnerStyle.spinner_mouth} />
+      <img src={spinnerBase} alt="spinner" className={spinnerStyle.spinner_base} />
+    </div>
+  </div>
+);
