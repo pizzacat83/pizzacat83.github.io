27288f9f1edaef9101739fa29ec47ea1608b54b7
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2020-03-13 10:47:41 +0900

    refactoring

--- src/components/spinner/index.tsx
+++ src/components/spinner/index.tsx
@@ -1,24 +1,20 @@
 import React, { CSSProperties } from "react";
-import spinnerBase from "./spinner_base.svg";
-import spinnerMouth from "./spinner_mouth.svg";
-import spinnerStyle from "./spinner.module.css";
+import baseImg from "./base.svg";
+import mouthImg from "./mouth.svg";
+import styles from "./styles.module.css";
 
 export const Spinner = ({ size }: { size: string }) => (
-  <div className={spinnerStyle.spinner_container} style={{ height: size }}>
-    <div className={spinnerStyle.spinner_mouth_outer}>
-      <img src={spinnerMouth} alt="" className={spinnerStyle.spinner_mouth} />
+  <div className={styles.container} style={{ height: size }}>
+    <div className={styles.mouth_outer}>
+      <img src={mouthImg} alt="" className={styles.mouth} />
     </div>
-    <img
-      src={spinnerBase}
-      alt="spinner"
-      className={spinnerStyle.spinner_base}
-    />
+    <img src={baseImg} alt="spinner" className={styles.base} />
   </div>
 );
 
 export const SpinnerContainer =
   ({ spinnerSize, style = {} }: { spinnerSize: string; style: CSSProperties }) => (
-    <div className={spinnerStyle.spinner_outer} style={style}>
+    <div className={styles.outer} style={style}>
       <Spinner size={spinnerSize} />
     </div>
   );
