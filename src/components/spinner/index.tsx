99403c67829791590acdb4d1c8c83e587b99b624
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2020-03-13 12:24:55 +0900

    modify animation

--- src/components/spinner/index.tsx
+++ src/components/spinner/index.tsx
@@ -1,5 +1,6 @@
 import React, { CSSProperties } from "react";
 import baseImg from "./base.svg";
+import eyesImg from "./eyes.svg";
 import mouthImg from "./mouth.svg";
 import styles from "./styles.module.css";
 
@@ -7,7 +8,9 @@ export const Spinner = ({ size }: { size: string }) => (
   <div className={styles.container} style={{ height: size }}>
     <div className={styles.mouth_outer}>
       <img src={mouthImg} alt="" className={styles.mouth} />
+      <img src={mouthImg} alt="" className={styles.mouth2} />
     </div>
+    <img src={eyesImg} alt="" className={styles.eyes} />
     <img src={baseImg} alt="spinner" className={styles.base} />
   </div>
 );
