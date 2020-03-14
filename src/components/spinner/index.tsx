df57b0143c0de1183ba53b0f06675574b2a23e14
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2020-03-14 17:52:25 +0900

    refactoring

--- src/components/spinner/index.tsx
+++ src/components/spinner/index.tsx
@@ -4,20 +4,27 @@ import eyesImg from "./eyes.svg";
 import mouthImg from "./mouth.svg";
 import styles from "./styles.module.css";
 
-export const Spinner = ({ size }: { size: string }) => (
+interface SpinnerConfig {
+  size: string;
+  rotate?: boolean;
+}
+
+export const Spinner = ({ size, rotate = true }: SpinnerConfig) => (
   <div className={styles.container} style={{ height: size }}>
-    <div className={styles.mouth_outer}>
-      <img src={mouthImg} alt="" className={styles.mouth} />
-      <img src={mouthImg} alt="" className={styles.mouth2} />
+    <div className={rotate ? styles.face_rotate : styles.face}>
+      <div className={styles.mouth_outer}>
+        <img src={mouthImg} alt="" className={styles.mouth} />
+        <img src={mouthImg} alt="" className={styles.mouth2} />
+      </div>
+      <img src={eyesImg} alt="" className={styles.eyes} />
+      <img src={baseImg} alt="spinner" className={styles.base} />
     </div>
-    <img src={eyesImg} alt="" className={styles.eyes} />
-    <img src={baseImg} alt="spinner" className={styles.base} />
   </div>
 );
 
 export const SpinnerContainer =
-  ({ spinnerSize, style = {} }: { spinnerSize: string; style: CSSProperties }) => (
+  ({ spinnerConfig, style = {} }: { spinnerConfig: SpinnerConfig; style: CSSProperties }) => (
     <div className={styles.outer} style={style}>
-      <Spinner size={spinnerSize} />
+      <Spinner {...spinnerConfig} />
     </div>
   );
