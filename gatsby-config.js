7049286cffa11e6ed03162757902f31150c22b4a
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2020-03-14 18:14:37 +0900

    use SCSS

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  plugins: ["gatsby-plugin-typescript", "gatsby-plugin-react-helmet"],
+  plugins: ["gatsby-plugin-typescript", "gatsby-plugin-react-helmet", "gatsby-plugin-sass"],
 };
